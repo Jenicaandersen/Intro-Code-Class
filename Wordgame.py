07f53d5aa5a4e51a854c1a603f8d77152943164b
Jenica Andersen <jenicaandersen@Jenica-Andersens-MacBook-Pro.local>
2016-09-25 14:28:58 -0600

    added the playing loop and some other stuff

--- Wordgame.py
+++ Wordgame.py
@@ -1,6 +1,36 @@
-def playwordgames():
-    print ("hey world")
+def get_random_word():
+    return "pizza"
+
+def play_word_games():
+    strikes = 0
+    max_strikes = 3 #this is creating variables
+    playing = True #boolean
+    #now we're going to create a loop to allow us to keep playing
+
+    word  = get_random_word() #defining new function needs paranthesis, then I went up and defined this get_random_word
+
+    while playing:  #this means it will continue running while this is true. Might also say while strikes < 3
+        strikes += 1 #taking strikes and and setting it equal to strikes + 1
+
+        if strikes >= max_strikes:
+            playing = False
+    #now our indent returns to the while place, so that we know where we're ending the while loop
+    if strikes >= max_strikes:
+        print("Sorry. Game Over.")
+    else:
+        print("Winner!") #ran and tested the code here. Good habit to keep testing
 
 
-print("playing")
-playwordgames()
+
+     
+
+
+
+
+
+
+
+
+print("Start the Game")
+play_word_games()
+print("End the Game")
